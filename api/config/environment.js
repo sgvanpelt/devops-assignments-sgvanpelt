e688be2fc244f933bc2143254cf312536f9e2be3
Bas van Pelt <49717976+sgvanpelt@users.noreply.github.com>
2021-04-03 15:58:37 +0200

    api omgevingsvariablen

--- api/config/environment.js
+++ api/config/environment.js
@@ -1,10 +1,10 @@
-const username = '';
-const password = '';
-const host = 'localhost';
-const port = '27017';
-const database = 'Connections';
-const params = '';
-const secret = 'MyLittleSecret';
+const username = process.env.db_username || '';
+const password = process.env.db_password || '';
+const host = process.env.DB || 'localhost';
+const port = process.env.port || '27017';
+const database = process.env.database || 'Connections';
+const params = process.env.params || '';
+const secret = process.env.secret || 'MyLittleSecret';
 
 let uri = 'mongodb://';
 if (username && password) {
